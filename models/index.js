7ac4496814b33a1e110be6e33de7705f6aed1e5f
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2023-01-27 22:57:44 -0700

    database seeding

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
-const { combineTableNames } = require('sequelize/types/utils');
+
 
 User.hasMany(Blog, {
     foreignKey: 'user_id',
